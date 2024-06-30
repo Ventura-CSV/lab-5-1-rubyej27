4069cc0ed7168548bf206d305a81d1488672b285
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-06-30 17:57:32 -0400

    commit

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ def getinput():
     # ******************************
     # Make your Code
     # ******************************
+    num = int(input('Enter number here: '))
+    return num
 
 
 def getsum(v1, v2):
     # ******************************
     # Make your Code
     # ******************************
+    total = v1 + v2
+    return total
 
 
 def printval(v1, v2, total):
     # ******************************
     # Make your Code
     # ******************************
+    print (v1, v2, total)
 
 
 def main():
